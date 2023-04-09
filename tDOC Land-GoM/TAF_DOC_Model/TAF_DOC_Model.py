4aa053b2852d9c96fcf276e8618b1ec8f7bce255
xinyuan_19 <40672320+xinyuanwylb19@users.noreply.github.com>
2023-04-09 15:32:19 -0400

    Update TAF_DOC_Model.py

--- tDOC Land-GoM/TAF_DOC_Model/TAF_DOC_Model.py
+++ tDOC Land-GoM/TAF_DOC_Model/TAF_DOC_Model.py
@@ -1,4 +1,5 @@
-# Terrestrial-aquatic DOC fluxes model (TAF-DOC)
+# Terrestrial-aquatic DOC fluxes model 
+# TAF-DOC
 # Version 2
 # Daliy simulation time step
 # Xinyuan Wei
@@ -9,73 +10,87 @@ import numpy as np
 import math
 
 # Read the diver data.
-soiloc=pd.read_csv('Driver_SOC.csv')
-wtland=pd.read_csv('Driver_Wetland.csv')
-opwate=pd.read_csv('Driver_Openwater.csv')
-watrew=pd.read_csv('Driver_WRTW.csv')
-watred=pd.read_csv('Driver_WRTD.csv')
+'''
+For short term simulations, the nitrogen and sulfer depostion can be used 
+to characterize the spatial patterns of DOC export.
+'''
 
-temp=pd.read_csv('Driver_Temp.csv')
-prep=pd.read_csv('Driver_Prep.csv')
-SDep=pd.read_csv('Driver_SDep.csv')
-NDep=pd.read_csv('Driver_NDep.csv')
+soiloc = pd.read_csv('Driver_SOC.csv')
+wtland = pd.read_csv('Driver_Wetland.csv')
+opwate = pd.read_csv('Driver_Openwater.csv')
+watrew = pd.read_csv('Driver_WRTW.csv')
+watred = pd.read_csv('Driver_WRTD.csv')
+
+temp = pd.read_csv('Driver_Temp.csv')
+prep = pd.read_csv('Driver_Prep.csv')
+SDep = pd.read_csv('Driver_SDep.csv')
+NDep = pd.read_csv('Driver_NDep.csv')
 
 # The total number of days.
-tdays=90
+tdays = 90
 
 # The total number of watersheds.
-twtsd=len(wtland)
+twtsd = len(wtland)
 
 # Outout results
-DOC_Expt=[]
-DOC_Burl=[]
-DOC_Minl=[]
+# DOC exported, DOC buried, and DOC mineralized.
+DOC_Expt = []
+DOC_Burl = []
+DOC_Minl = []
 
 ########################################
 # Parameters
 ########################################
-# Soil DOC parameters
+# Soil DOC parameters / DOC density
+# Desorption model
 sp1=0.1356
 sp2=0.956
 
 # Watershed DOC loading parameters
-wp0=1.2369
-wp1=-0.0072
-wp2=0.0047
-wp3=-0.4039
-wp4=0.3055
-wpe=-0.12073
+# Coefficients for environmental factors.
+# Wetland fraction, temperature, precipitation
+# nitrogen deposition, sulfur deposition
+wp0 = 1.2369
+wp1 = -0.0072
+wp2 = 0.0047
+wp3 = -0.4039
+wp4 = 0.3055
 
-wpp0=1.2361
-wpp1=0.0053
-wpp2=-0.1976
+# The lagged effect parameter
+wpe = -0.12073
 
-MaDOC=15
-MiDOC=1.76
 
-burp1=0.0896
-burp2=0.0121
+wpp0 = 1.2361
+wpp1 = 0.0053
+wpp2 = -0.1976
 
-minp1=0.1563
+MaDOC = 15
+MiDOC = 1.76
+
+burp1 = 0.0896
+burp2 = 0.0121
+
+minp1 = 0.1563
 
 # Delivery process.
-dbp1=0.0005
-dbp2=148.61
+dbp1 = 0.0005
+dbp2 = 148.61
 
-dmp1=1.441
+dmp1 = 1.441
 
 # Maximum DOC export. Model results constratin g/m2
-MDOC=15
+# This parameter is obtained from measurement.
+MDOC = 15  
 
 print ('The total number of watershed is',twtsd,'.')
 print ('The total number of simulated days is',tdays,'.')
 
 ########################################
-# SDOCM, Soil DOC concentration.
-# The DOC is g/m2.
+# Soil DOC concentration model.
+# The soil DOC density is g/m2.
 ########################################
 def SDOCM(SOC):
-    SDOC=sp1*sp2*SOC
+    SDOC = sp1*sp2*SOC
     return(SDOC)
 
 ########################################
@@ -85,79 +100,85 @@ def SDOCM(SOC):
 def WFDOCM(W, T, P, S, N, OW, WRW, WRD):
     # DOC export from a watershed.
     # If some environmental factors are not available. 
-    if T!=0 and S!=0 and N!=0:
-        DOCE=wp0*W+wp1*T+wp2*P+wp3*S+wp4*N+wpe
+    if T != 0 and S != 0 and N != 0:
+        DOCE = wp0 * W + wp1 * T + wp2 * P + wp3 * S + wp4 * N + wpe
         
-    # Only pricipitation
+    # Only use precipitaion
     else:
-        DOCE=wpp0*W+wpp1*P+wpp2
+        DOCE = wpp0 * W + wpp1 * P + wpp2
         
-    # Outliers
-    if DOCE>MaDOC:
-        DOCE=MaDOC
+    # Detect outliers
+    if DOCE > MaDOC:
+        DOCE = MaDOC
         
-    if DOCE<0:
-        DOCE=MiDOC
+    if DOCE < 0:
+        DOCE = MiDOC
 
-    # DOC buried within the watershed. 
-    DOCB=burp1*math.exp(burp2*OW)*DOCE
+    # DOC buried in inland waters within the watershed. 
+    DOCB = burp1 * math.exp(burp2 * OW) *DOCE
     
-    # DOC mineralized within the watershed.
-    DOCM=minp1*WRW*DOCE
+    # DOC mineralized in inland waters within the watershed.
+    DOCM = minp1 * WRW * DOCE
     
     # DOC buried and mineralized during the delivery process.
-    DOCEB_r=dbp1*math.exp(dbp2*WRD)*DOCE
-    DOCEM_r=dmp1*WRD*DOCE
+    # From the watershed outlet to the ocean.
+    DOCEB_r = dbp1 * math.exp(dbp2 * WRD) *DOCE
+    DOCEM_r = dmp1 * WRD * DOCE
     
-    if (DOCEB_r+DOCEM_r)>0.17:
-        DOCEB_r=0.06
-        DOCEM_r=0.11
+    if (DOCEB_r + DOCEM_r) > 0.17:
+        DOCEB_r = 0.06
+        DOCEM_r = 0.11
 
-    DOCEB=DOCE*DOCEB_r
-    DOCEM=DOCE*DOCEM_r
+    DOCEB = DOCE * DOCEB_r
+    DOCEM = DOCE * DOCEM_r
     
-    DOCBB=DOCB+DOCEB
-    DOCMM=DOCM+DOCEM
-    DOCEE=DOCE-DOCEB-DOCEM
+    # The total buried DOC
+    # The total mineralized DOC
+    # The total DOC delivered to the ocean
+    DOCBB = DOCB + DOCEB
+    DOCMM = DOCM + DOCEM
+    DOCEE = DOCE - DOCEB - DOCEM
     
-    return(DOCBB,DOCMM,DOCEE)
+    return(DOCBB, DOCMM, DOCEE)
 
-# Estimation for each watershed.
+# Iterate the estimation for each watershed.
 for i in range (twtsd):
     
-    print('Calculation for the watershed:',str(i+1))
-    wtsd_ID=i
+    print('Calculation for the watershed:', str(i+1))
+    wtsd_ID = i
     
-    WT=wtland.at[wtsd_ID,'Wetland']
-    OW=opwate.at[wtsd_ID,'Openwater']
+    WT = wtland.at[wtsd_ID, 'Wetland']
+    OW = opwate.at[wtsd_ID, 'Openwater']
     
-    WRT_W=watrew.at[wtsd_ID,'WRTW']
-    WRT_D=watred.at[wtsd_ID,'WRTD']
+    WRT_W = watrew.at[wtsd_ID,'WRTW']
+    WRT_D = watred.at[wtsd_ID,'WRTD']
     
-    SOC=soiloc.at[wtsd_ID,'SOC']
+    SOC = soiloc.at[wtsd_ID,'SOC']
     
     # Soil DOC.
-    SDOC=SDOCM(SOC)
+    SDOC = SDOCM(SOC)
 
-    temp_DOCB=[]
-    temp_DOCM=[]
-    temp_DOCE=[]
+    temp_DOCB = []
+    temp_DOCM = []
+    temp_DOCE = []
 
+    # Estimation for each day.
     for j in range (tdays):
-        Tj=float(temp.iloc[i,j])
-        Pj=float(prep.iloc[i,j])
-        Sj=float(SDep.iloc[i,j])
-        Nj=float(NDep.iloc[i,j])
         
-        DOC_B=WFDOCM(WT, Tj, Pj, Sj, Nj, OW, WRT_W, WRT_D)[0]
-        DOC_M=WFDOCM(WT, Tj, Pj, Sj, Nj, OW, WRT_W, WRT_D)[1]
-        DOC_E=WFDOCM(WT, Tj, Pj, Sj, Nj, OW, WRT_W, WRT_D)[2]
+        Tj = float(temp.iloc[i, j])
+        Pj = float(prep.iloc[i, j])
+        Sj = float(SDep.iloc[i, j])
+        Nj = float(NDep.iloc[i, j])
+        
+        DOC_B = WFDOCM(WT, Tj, Pj, Sj, Nj, OW, WRT_W, WRT_D)[0]
+        DOC_M = WFDOCM(WT, Tj, Pj, Sj, Nj, OW, WRT_W, WRT_D)[1]
+        DOC_E = WFDOCM(WT, Tj, Pj, Sj, Nj, OW, WRT_W, WRT_D)[2]
         
         # DOC loading should be less than SDOC
-        if (DOC_B+DOC_M+DOC_E)>SDOC:
-            DOC_B=0.1396*SDOC
-            DOC_M=0.2563*SDOC   
-            DOC_E=0.3563*SDOC
+        if (DOC_B + DOC_M+DOC_E) > SDOC:
+            DOC_B = 0.1396 * SDOC
+            DOC_M = 0.2563 * SDOC   
+            DOC_E = 0.3563 * SDOC
             
         temp_DOCB.append(DOC_B)
         temp_DOCM.append(DOC_M)
